1ae62b23c10318b0c5e1e455fa659e560fa9d749
Nikolay Ivanov <ivanov.nikolay.main@gmail.com>
2018-01-15 07:02:08 +0300

    Direction number params removed

--- src/sobolseqgenerator.cpp
+++ src/sobolseqgenerator.cpp
@@ -1,55 +1,111 @@
-#include <cstdlib>
-#include <cmath>
-#include <iostream>
-#include <iomanip>
-#include <fstream>
-#include <string>
+// Frances Y. Kuo
+//
+// Email: <f.kuo@unsw.edu.au>
+// School of Mathematics and Statistics
+// University of New South Wales
+// Sydney NSW 2052, Australia
+//
+// Last updated: 21 October 2008
+//
+//   You may incorporate this source code into your own program
+//   provided that you
+//   1) acknowledge the copyright owner in your program and publication
+//   2) notify the copyright owner by email
+//   3) offer feedback regarding your experience with different direction numbers
+//
+//
+// -----------------------------------------------------------------------------
+// Licence pertaining to sobol.cc and the accompanying sets of direction numbers
+// -----------------------------------------------------------------------------
+// Copyright (c) 2008, Frances Y. Kuo and Stephen Joe
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+//     * Neither the names of the copyright holders nor the names of the
+//       University of New South Wales and the University of Waikato
+//       and its contributors may be used to endorse or promote products derived
+//       from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// -----------------------------------------------------------------------------
+
 #include "sobolseqgenerator.h"
 
-int SobolSeqGenerator::Init(uint32_t N_, uint32_t D_, std::string dir_file){
-    N = N_;
-    D = D_;
+int SobolSeqGenerator::Init(uint32_t _N, uint32_t _D, std::string dir_file){
+    int i,j,k;
+    N = _N;
+    D = _D;
     current_point_number = -1;
-    params_filename = dir_file;
 
-    L = (unsigned)ceil(log((double)N)/log(2.0));
-
-    std::ifstream infile(params_filename,std::ios::in);
+    std::ifstream infile(dir_file,std::ios::in);
     if (!infile) {
         std::cout << "Input file containing direction numbers cannot be found!" << std::endl;
         exit(1);
     }
 
+    L = (unsigned)ceil(log((double)N)/log(2.0));
+    for (i = 0; i < D; i++){
+        V.emplace_back(L + 1);
+        for (j = 0; j <= L; j++){
+            V[i].push_back(0);
+        }
+    }
+
     char buffer[1000];
     infile.getline(buffer,1000,'\n');
 
-    dir_num_params.push_back(DirectionalNumbersParams());
+    for (i = 1; i < D; i++){
+        // Read parameters from file
+        unsigned s, a;
+        infile >> s >> s >> a;
+        std::vector<unsigned> m;
+        m.push_back(1);
+        for (j = 1; j <= s; j++){
+            unsigned _m;
+            infile >> _m;
+            m.push_back(_m);
+        }
 
-    for (unsigned i = 1; i < D; i++)
-    {
-        dir_num_params.push_back(DirectionalNumbersParams());
-        // Чтение параметров из файла
-        unsigned d_, s_, a_;
-        infile >> d_ >> s_ >> a_;
-        dir_num_params[i].d = d_;
-        dir_num_params[i].s = s_;
-        dir_num_params[i].a = a_;
-        dir_num_params[i].m.push_back(1);
-        for (unsigned j = 1; j <= s_; j++)
-        {
-            unsigned m_;
-            infile >> m_;
-            dir_num_params[i].m.push_back(m_);
+        // Compute direction numbers
+        if (L <= s){
+            for (j = 1; j <= L; j++){
+                V[i][j] = m[j] << (32 - j);
+            }
+        }
+        else {
+            for (j = 1; j <= s; j++){
+                V[i][j] = m[j] << (32 - j);
+            }
+            for (j = s + 1; j <= L; j++) {
+                V[i][j] = V[i][j - s] ^ (V[i][j - s] >> s);
+                for (k = 1; k <= s - 1; k++)
+                    V[i][j] ^= (((a >> (s - 1 - k)) & 1) * V[i][j - k]);
+            }
         }
     }
     infile.close();
-
     return 0;
 }
 
 PointReal SobolSeqGenerator::GeneratePoint(){
-    int i,j,k;
-
     if (N == 0 || D == 0){
         std::cout << "Wrong initialization! N = " << N << ", D = " << D << std::endl;
         return PointReal();
@@ -62,9 +118,8 @@ PointReal SobolSeqGenerator::GeneratePoint(){
 
     current_point_number++;
 
-    if(current_point_number == 0)
-    {
-        //The 0-th point is zero
+    if(current_point_number == 0){
+        //The 0-th point is (0, 0, ..., 0)
         last_generated_point = PointUnsigned(D, std::vector<unsigned>(D, 0));
         return PointReal(D, std::vector<Real>(D, 0));
     }
@@ -78,42 +133,18 @@ PointReal SobolSeqGenerator::GeneratePoint(){
         C++;
     }
 
-    // Compute direction numbers V, scaled by pow(2,32)
+    // Compute first dimension
     int v = 1 << (32 - C);
-    temp_point.coordinate[0] = last_generated_point.coordinate[0] ^ v;
+    last_generated_point.coordinate[0] = last_generated_point.coordinate[0] ^ v;
 
-    // ----- Compute the remaining dimensions -----
-    for (i = 1; i < D; i++){
-        unsigned s = dir_num_params[i].s;
-        unsigned a = dir_num_params[i].a;
-        auto m     = dir_num_params[i].m;
-        // Compute direction numbers V[1] to V[L], scaled by pow(2,32)
-        std::vector<unsigned> V(L+1);
-
-        if (L <= s){
-            for (j = 1; j <= L; j++){
-                V[j] = m[j] << (32 - j);
-            }
-        }
-        else {
-            for (j = 1; j <= s; j++){
-                V[j] = m[j] << (32 - j);
-            }
-            for (j = s + 1; j <= L; j++) {
-                V[j] = V[j - s] ^ (V[j - s] >> s);
-                for (k = 1; k <= s - 1; k++)
-                    V[j] ^= (((a >> (s - 1 - k)) & 1) * V[j - k]);
-            }
-        }
-
-        temp_point.coordinate[i] = last_generated_point.coordinate[i] ^ V[C];
+    // Compute the remaining dimensions
+    for (int i = 1; i < D; i++){
+        last_generated_point.coordinate[i] = last_generated_point.coordinate[i] ^ V[i][C];
     }
 
-    last_generated_point = temp_point;
-
     PointReal final_result = PointReal(D);
-    for (i = 0; i < D; i++){
-        final_result.coordinate[i] = (Real) temp_point.coordinate[i] / std::pow(Real(2.0), 32);
+    for (int i = 0; i < D; i++){
+        final_result.coordinate[i] = (Real) last_generated_point.coordinate[i] / std::pow(Real(2.0), 32);
     }
 
     return final_result;
